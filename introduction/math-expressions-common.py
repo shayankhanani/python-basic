ebb1636ed25798145e403e3f474ba1bbc0dcd2f2
shayankhanani <shayan.khanani@outlook.com>
2019-08-04 13:05:04 +0500

    The brief overview of some common mathematical functions

---
+++ introduction/math-expressions-common.py
@@ -0,0 +1,25 @@
+pop_number = 3
+
+#addition - assigning expression result
+pop_number = 3 + 2
+print(pop_number)
+
+#subtraction
+loss = 12 - 35
+print(loss)
+
+#multiplication
+converter_m_cm = 12 * 100
+print(converter_m_cm) 
+
+#division
+converter_cm_m = 55 / 100
+print(converter_cm_m)
+
+#general calculation
+num = 99
+to_operate = 35.5
+calc = num + to_operate
+print(calc)
+calc = num - to_operate
+print(calc)
