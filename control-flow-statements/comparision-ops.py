ac5ecd26378d1541aa58e7b28d3f219e341b8093
shayankhanani <shayan.khanani@outlook.com>
2019-08-10 00:02:16 +0500

    comparision operators check and compare values and return true or false accordingly
    == : Equality checker whether the value is equal to the l.h.s
    != : Non Equality checker whether the value is not equal to the l.h.s
    <= : Less than checker whether the value is less than or equal to the
    r.h.s
    >= : Greater than checker whether the value is greater than or equal to
    the r.h.s
    < : Less than checker whether the value is less than to the r.h.s
    > : Greater than checker whether the value is greater than to the r.h.s

---
+++ control-flow-statements/comparision-ops.py
@@ -0,0 +1,17 @@
+x = 3
+y = 7
+z = 9
+a = 3
+
+if y > x:
+    print("True y is greater than x")
+if x < y:
+    print("True x is less than y")
+if z >= y:
+    print("True z is greater than or equal to y")
+if x >= a:
+    print("True x is greater than or equal to a")
+if a <= y:
+    print("True a is less than or equal to y")
+if a <= x:
+    print("True a is less than or equal to x")
