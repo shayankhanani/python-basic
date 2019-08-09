df1add49b08190e5b1dfbc7378480c92deca9678
shayankhanani <shayan.khanani@outlook.com>
2019-08-09 23:50:14 +0500

    The code discuss: Modulo Operator: which returns remainder after division Short Hand Addition, Subtraction, Multiplication and division

---
+++ introduction/math-expressions-uncommon.py
@@ -0,0 +1,21 @@
+remainder = 9 % 3
+print(remainder)
+remainder = 10 % 3
+print("Modulo Operator: ")
+print(remainder)
+user = 32
+user = user + 21
+print("Long Addition")
+print(user)
+user += 21
+print("Shorthand Addition: ")
+print(user)
+user -= 42
+print("Shorthand Subtraction: ")
+print(user)
+user *= 2
+print("Shorthand Multiplication: ")
+print(user)
+user /= 3
+print("Shorthand Division: ")
+print(user)
