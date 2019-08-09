a20e7e194371ab93145935e0116c14fe30d9fe8c
shayankhanani <shayan.khanani@outlook.com>
2019-08-09 23:56:42 +0500

    Combining two or more strings by "+" operator note that string cant be concatenate with number unless the numbers are not string

---
+++ introduction/concatenating-strings.py
@@ -0,0 +1,11 @@
+greeting = "Hello"
+addressee = "World"
+print(greeting + " " +addressee)
+addressee = "Teacher"
+print(greeting + " " + addressee)
+
+
+separators = ", "
+punc = "!"
+whole_greeting = greeting + separators + addressee + punc
+print(whole_greeting)
