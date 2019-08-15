a374ff93a05735e247c1956ed3d225dd4cef909e
shayankhanani <shayan.khanani@outlook.com>
2019-08-15 22:44:45 +0500

    To check multiple condition we can use either set of conditions with logical operators or we can break into nested if-else-elif statements

---
+++ control-flow-statements/nested-conditions.py
@@ -0,0 +1,29 @@
+x = 4
+y = 4
+a = 3
+b = 5
+c = 5
+d = 5
+h = 8
+f = 11
+
+if (x == y or a == b) and c == d:
+    g = h
+    print(g)
+else:
+    e = f
+    print(e)
+
+if(c==d):
+    if(x == y):
+        g = h
+        print(g)
+    elif(a == b):
+        g == h
+        print(g)
+    else:
+        e = f
+        print(e)
+else:
+    e = f
+    print(e)
