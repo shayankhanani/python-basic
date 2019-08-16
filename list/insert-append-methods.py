96e24b2cb5b0dd25bead64a85d85d52fc08cfd79
shayankhanani <shayan.khanani@outlook.com>
2019-08-16 23:47:03 +0500

    append method of list use to insert element on its last position list.append(value) is general syntax
    insert method of list is use to insert element at specified position
    list.insert(index of element, value)

---
+++ list/insert-append-methods.py
@@ -0,0 +1,7 @@
+cities = ["Karachi","Lahore","Islamabad","Quetta","Pehawar"]
+cities.append("Hyderabad")
+print(cities[5])
+cities.insert(0,"Sawat")
+print(cities[0])
+cities.insert(2,"Sialkot")
+print(cities[2])
