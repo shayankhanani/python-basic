0bff9391afbf9a091225ef67f08ae66e080ea7e1
shayankhanani <shayan.khanani@outlook.com>
2019-08-16 23:51:38 +0500

    slice is a process of copying list element to another list list[index of element from where to start copy: index of element before you want to stop copying]

---
+++ list/list-slice.py
@@ -0,0 +1,5 @@
+cities = ["Karachi","Lahore","Islamabad","Quetta","Pehawar","Hyderabad","Sialkot","Gawadar","Mardan"]
+p_capitals = cities[:5]
+print(p_capitals[4])
+other_cities = cities[5:9]
+print(other_cities[3])
