2c45f93290f0d432e259091cd1657469de507f37
shayankhanani <shayan.khanani@outlook.com>
2019-08-16 23:42:32 +0500

    list is a variable that can have a sequence of values assigned to it which is known as element of list

---
+++ list/introduction-list.py
@@ -0,0 +1,9 @@
+city0 = "Karachi"
+city1 = "Lahore"
+city2 = "Islamabad"
+city3 = "Quetta"
+city4 = "Peshawar"
+print("Welcome to city " + city3)
+
+cities = ["Karachi","Lahore","Islamabad","Quetta","Pehawar"]
+print("Welcomw to city " + cities[4])
