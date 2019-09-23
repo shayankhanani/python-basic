dc90bc05b114b36553983e33b36d9b2679b03c35
shayankhanani <shayan.khanani@outlook.com>
2019-09-23 22:13:34 +0500

    Python allows to calls function within and a function and function which can provide arguments to a function
    the code consist of three function
    byeGreet which return the greeting
    name which return the name
    sayBye which print the bye message in which getName parameter is
    supplied by name method and greet variable gets it value from byeGreet
    method

---
+++ functions/function-within-function.py
@@ -0,0 +1,11 @@
+def byeGreet(say):
+    return say
+def sayBye(getName):
+    greet = byeGreet("Bye Bye!: ")
+    print(greet + getName)
+def name(value):
+    return value
+
+sayBye(name("Shayan"))
+
+    
