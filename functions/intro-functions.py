7c67ffb33ac193ac24f7b0a9a262da14ae448125
shayankhanani <shayan.khanani@outlook.com>
2019-09-23 20:58:36 +0500

    Function is a block of code which on call perform action.
    Function is define with def key word and statement inside the function
    are indented

---
+++ functions/intro-functions.py
@@ -0,0 +1,12 @@
+number1 = 24
+number2 = 19
+operation = number1 + number2
+print(str(operation))
+
+def addition():
+    number1 = 13
+    number2 = 14
+    operation = number1 + number2
+    print(str(operation))
+
+addition()
