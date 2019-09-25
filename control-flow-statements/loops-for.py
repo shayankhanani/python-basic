ca7216010d2e090492e2abf01a3d9dbcbdf2a9e1
shayankhanani <shayan.khanani@outlook.com>
2019-09-25 22:42:59 +0500

    Too repeat the specific command we create loops
    for loop in python help us to iterate through the list and object
    
    The code iterate through the touple of states and print the most
    populated state which is specified populated_state variable

---
+++ control-flow-statements/loops-for.py
@@ -0,0 +1,5 @@
+states_of_pakistan = ("Balochistan","Gilgit Baltistan","Khyber Pakhtunkhwa","Sindh","Punjab")
+populated_state = states_of_pakistan[4]
+for pop_state in states_of_pakistan:
+    if pop_state == populated_state:
+        print("Populated state found: " + pop_state)
