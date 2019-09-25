a5a8f95b5a5307dc5e3ba30c722f2aedf6de277f
shayankhanani <shayan.khanani@outlook.com>
2019-09-25 23:02:50 +0500

    New item in a dictionary can be specified via its key in square brackets along with the dictionary name.
    Removing item from a dictionary can be carried via del keyword along
    with the key of dictionary item which we want to delete

---
+++ dictionary/dictionary-add-remove-item.py
@@ -0,0 +1,6 @@
+todaysCustomer = {"name":"Mark", "email":"mark@zuckerberg.com", "city":"San Francisco", "telephone": 92321321321}
+print(todaysCustomer)
+todaysCustomer['purchase'] = "User's Data"
+print(todaysCustomer)
+del todaysCustomer['purchase']
+print(todaysCustomer)
