c40f09c4f932d47156a3231db8e02dff2d4196d8
shayankhanani <shayan.khanani@outlook.com>
2019-09-25 22:55:37 +0500

    Dictionary in python is an ordered collection of items
    A dictionary has a key: value pair
    
    We can access any item of dictionary by calling out its key specified in
    square bracket with dictionary name
    
    There is no restriction in the data type of key in Dictionary it can be
    string or it can be a number

---
+++ dictionary/dictionary-python.py
@@ -0,0 +1,11 @@
+todaysCustomer = {"name":"Mark", "email":"mark@zuckerberg.com", "city":"San Francisco", "telephone": 92321321321}
+print(todaysCustomer['telephone'])
+rankings = {5: "Finland", 2: "Norway", 3: "Sweden", 7: "Iceland"}
+print(rankings[3])
+things_to_remember = {
+    0: "the lowest number",
+    "a dozen": 12,
+    "snake eyes": "a pair of ones",
+    13: "a baker's dozen"
+    }
+print(things_to_remember[0] + ' ' + str(things_to_remember["a dozen"]))
